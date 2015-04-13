5e658f75bb078bbcc13850019205cc26176d7afa
mofd <konstantin.steuer@email.de>
2015-04-13 15:15:11 +0200

    Step 2 Angelegt: * Application.java * index.html

---
+++ src/main/java/de/swm/example/Application.java
@@ -0,0 +1,18 @@
+package de.swm.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO (KS) Dokumentieren.
+ *
+ * @author steuer.konstantin <br>
+ * copyright (C) 2015, SWM Services GmbH
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
