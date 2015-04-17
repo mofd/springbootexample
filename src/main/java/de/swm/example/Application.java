e0f9e043937920ca180dddaf1f0e05949c132730
mofd <technology@email.de>
2015-04-17 19:58:20 +0200

    Step 10 * Umkonfigurieren von jar auf war

--- src/main/java/de/swm/example/Application.java
+++ src/main/java/de/swm/example/Application.java
@@ -2,6 +2,8 @@ package de.swm.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * TODO (KS) Dokumentieren.
@@ -10,7 +12,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * copyright (C) 2015, SWM Services GmbH
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
